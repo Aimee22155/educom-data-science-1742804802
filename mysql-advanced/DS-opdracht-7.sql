bac9fa27e568347448d449d666b56fb883270ad9
Aimee22155 <heusschenaimee@gmail.com>
2025-03-24 14:33:00 +0100

    Update DS-opdracht-7.sql

--- mysql-advanced/DS-opdracht-7.sql
+++ mysql-advanced/DS-opdracht-7.sql
@@ -3,7 +3,8 @@ ALTER TABLE employees
 ADD (salary FLOAT, date_hired DATE, date_fired DATE);
 
 ----2----
-INSERT INTO employees (salary, date_hired, date_fired)
-VALUES ('2500', '2025-01-24', '2025-03-24');
+UPDATE employees
+SET salary = '1000'
+WHERE officeCode BETWEEN 1 AND 7;
 
 ----3----
