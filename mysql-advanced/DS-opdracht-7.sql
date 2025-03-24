f7c3b046a6f015fbeb985c0714863c628c47958b
Aimee22155 <heusschenaimee@gmail.com>
2025-03-24 14:09:33 +0100

    Update DS-opdracht-7.sql

--- mysql-advanced/DS-opdracht-7.sql
+++ mysql-advanced/DS-opdracht-7.sql
@@ -1,2 +1,9 @@
+----1----
 ALTER TABLE employees
 ADD (salary FLOAT, date_hired DATE, date_fired DATE);
+
+----2----
+INSERT INTO employees (salary, date_hired, date_fired)
+VALUES ('2500', '2025-01-24', '2025-03-24');
+
+----3----
