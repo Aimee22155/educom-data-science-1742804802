49d2df41b26cd659063ba0b4fe273d7788544831
Aimee22155 <heusschenaimee@gmail.com>
2025-04-10 09:26:28 +0200

    Case_notebook.py

---
+++ Case_notebook.py
@@ -0,0 +1,38 @@
+Globale opzet voor opschonen data
+________________________________
+import pandas as pd
+import sqlalchemy as SA
+________________________________
+engine = SA.create_engine(f"mysql+pymysql://root@localhost/climate_watch?charset=utf8mb4")
+________________________________
+countries= 'countries'
+cities= 'cities'
+states= 'states'
+country_temp= 'country_temperatures'
+city_temp= 'city_temperatures'
+state_temp= 'state_temperatures'
+global_temp= 'global_temperatures'
+________________________________
+df = pd.read_sql_table(countries, engine)
+print(df.head())
+_______________________________
+print("\n--- Controle op ontbrekende waarden ---")
+print("Aantal ontbrekende waarden per kolom:")
+print(df.isnull().sum())
+
+print("\nPercentage ontbrekende waarden per kolom:")
+print(df.isnull().mean() * 100)
+______________________________
+print("\n--- Vervangen van ontbrekende waarden door het gemiddelde ---")
+for column in df.columns:
+    if df[column].isnull().any(): # Controleer of de kolom ontbrekende waarden heeft
+        if pd.api.types.is_numeric_dtype(df[column]): # Alleen voor numerieke kolommen
+            gemiddelde = df[column].mean()
+            df[column].fillna(gemiddelde, inplace=True)
+            print(f"Ontbrekende waarden in kolom '{column}' vervangen door het gemiddelde: {gemiddelde:.2f}")
+        else:
+            print(f"Kolom '{column}' bevat ontbrekende waarden maar is niet numeriek. Overweeg een andere strategie.")
+
+print("\nDataFrame na het vervangen van ontbrekende numerieke waarden door het gemiddelde:")
+print(df.head())
+____________________________
